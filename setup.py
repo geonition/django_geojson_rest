c179dd2c42084adfbebab37920e0ba56e9c75f97
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-24 22:44:49 +0300

    setup script added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='geojson_rest',
+    version='1.0.1',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_geojson_rest',
+    packages=['geojson_rest'],
+)
