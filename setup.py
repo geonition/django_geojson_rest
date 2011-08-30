89e194fc5986e6f8b0ff1186513ed2074e63def2
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-30 16:31:14 +0300

    the setup is modified

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from distutils.core import setup
-
-setup(
-    name='geojson_rest',
-    version='1.0.1',
-    author='Kristoffer Snabb',
-    url='https://github.com/geonition/django_geojson_rest',
-    packages=['geojson_rest'],
-)
-
 from setuptools import setup
 from setuptools import find_packages
 
