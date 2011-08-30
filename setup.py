30714221012b4611135d507b6c82c9d94c14b951
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-30 16:26:08 +0300

    setip modified to include template files

--- setup.py
+++ setup.py
@@ -7,3 +7,23 @@ setup(
     url='https://github.com/geonition/django_geojson_rest',
     packages=['geojson_rest'],
 )
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='geojson_rest',
+    version='1.0.0',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_geojson_rest',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        "geojson_rest": [
+            "templates/*.js",
+            "templates/*.html"
+        ],
+    },
+    zip_safe=False,
+    install_requires=['django']
+)
