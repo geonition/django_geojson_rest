d880a87d81d460936d840354150dae7d71fa2897
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-24 14:07:22 +0300

    setup script updated so that it has the right version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geojson_rest',
-    version='1.0.0',
+    version='2.0.x',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geojson_rest',
     packages=find_packages(),
