56cb5febb8ef4cf026e8290961df91af2d583b67
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-05-30 14:48:43 +0300

    admin.py changed so that the field cannot be modified readonly

--- geojson_rest/admin.py
+++ geojson_rest/admin.py
@@ -1,8 +1,24 @@
-from django.contrib.gis import admin as gis_admin
 from django.contrib import admin
+from django.conf import settings
 from models import Feature
 from models import Property
 
-admin.site.register(Feature, gis_admin.OSMGeoAdmin)
-admin.site.register(Property, admin.ModelAdmin)
+
+class FeatureAdmin(admin.ModelAdmin):
+    readonly_fields = ('geometry',
+                        'user',
+                        'group',
+                        'private',
+                        'properties',
+                        'time')
+    
+
+class PropertyAdmin(admin.ModelAdmin):
+    readonly_fields = ('user',
+                       'group',
+                       'json_data',
+                       'time')
+    
+admin.site.register(Feature, FeatureAdmin)
+admin.site.register(Property, PropertyAdmin)
 
