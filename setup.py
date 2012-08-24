ec3af5aef2882d268ba7d121dcef223c6fb29f7d
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-08-24 12:56:18 +0300

    increased version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geojson_rest',
-    version='5.2.2',
+    version='5.3.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geojson_rest',
     packages=find_packages(),
