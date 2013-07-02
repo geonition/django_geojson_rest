b2b6c3ea528eeed1c4c9f896bae73722aa165a9b
Mikko Johansson <mikko.johansson@aalto.fi>
2013-07-02 12:49:10 +0300

    removed sql folder as it is not needed anymore

--- geojson_rest/sql/polygonfeature.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW PolygonFeature AS SELECT * FROM geojson_rest_feature WHERE GeometryType(geometry) = 'POLYGON';
-CREATE OR REPLACE VIEW PolygonFeatureProperty 
-AS SELECT * FROM geojson_rest_feature_properties 
-WHERE feature_id 
-IN (SELECT id FROM PolygonFeature);
