02e8c332bde30fc45dec0326f0bfb9f41b5740ab
Sakari Ellonen <sakari.ellonen@gmail.com>
2013-10-31 13:10:14 +0200

    doh what a bug

--- geojson_rest/utils.py
+++ geojson_rest/utils.py
@@ -2,8 +2,8 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 
 def send_error_mail(request,msg):
-    msg += '\n\nrequest.user: ' + request.user
-    msg += '\n\nrequest.path: ' + request.path
+    msg += '\n\nrequest.user: ' + str(request.user)
+    msg += '\n\nrequest.path: ' + str(request.path)
     title = 'Geonition error'
     mail = EmailMessage(title, msg, to=[getattr(settings,'ADMINS',(('master','webmaster@mapita.fi'),))[0][1]])
     mail.send()
